8c1b665b3c1e2ed1dd5d350be78a97414064f3b3
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2024-01-26 13:04:02 +0000

    Draft commit.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker build --no-cache -t hide-server .
-# docker run -dit -p 80:80 --name hide-server hide-server
+# docker run -dit -p 80:80 -p 8080:8080 --name hide-server hide-server
 docker-compose up -d
